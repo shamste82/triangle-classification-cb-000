13582ee90a94338745b6701c294ed1633a69b559
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-19 14:24:23 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+
+  attr_accessor :sideA, :sideB, :sideC
+
+  def initialize(sideA, sideB, sideC)
+    @sideA = sideA
+    @sideB = sideB
+    @sideC = sideC
+  end
+
+  def kind
+    if @sideA == 0 and @sideB == 0 and @sideC == 0
+      raise TriangleError
+    elsif @sideA < 0 or @sideB < 0 or @sideC < 0
+      raise TriangleError
+    elsif @sideA + @sideB <= @sideC or @sideA + @sideC <= @sideB or @sideB + @sideC <= @sideA
+      raise TriangleError
+    elsif self.sideA == self.sideB and self.sideB == self.sideC
+      :equilateral
+    elsif self.sideA == self.sideB or self.sideA == self.sideC or self.sideB == self.sideC
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+end
+
+
+class TriangleError < StandardError
+  def triangle_no_size
+    "The triangles sides must be greater than 0"
+  end
+
 end
